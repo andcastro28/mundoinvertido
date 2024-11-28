95774bd248445e0e1fc81bf99e155c166bd678a9
andcastro28 <andcastro28@gmail.com>
2024-11-28 12:23:26 -0300

    prmeiro commit mundo invertido

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+window.addEventListener('click', function() {
+    const audio = document.getElementById('music');
+    audio.play();
+    audio.volume = 0.2;
+  })
+
+  const trocatema = document.getElementById('switch-theme-button')
+
+  trocatema.addEventListener('click', function() {
+    switchTheme();
+  })
+
+
+  function switchTheme() {
+    document.body.classList.toggle('dark-theme');
+    document.body.classList.toggle('light-theme');
+
+    const theme = document.body.classList[0];
+    const music = theme === 'light-theme' ? 'normal-world.mpeg' : 'inverted-world.mpeg'
+
+    const audio = document.getElementById('music');
+    audio.src = `assets/musics/${music}`;
+    audio.play();
+    audio.volume = 0.2;
+  }
